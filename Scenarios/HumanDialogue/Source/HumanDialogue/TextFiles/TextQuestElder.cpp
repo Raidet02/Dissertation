bb5d18d2f35d6a65ee8823e26ab48b587c69b88d
Raidet02 <146941600+Raidet02@users.noreply.github.com>
2026-01-22 16:40:35 +0000

    v2

--- Scenarios/HumanDialogue/Source/HumanDialogue/TextFiles/TextQuestElder.cpp
+++ Scenarios/HumanDialogue/Source/HumanDialogue/TextFiles/TextQuestElder.cpp
@@ -10,23 +10,23 @@ ATextQuestElder::ATextQuestElder()
 
 void ATextQuestElder::GetDialogue()
 {
-	ElderP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/ElderP1Dialogue.txt");
-	ElderP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/ElderP2Dialogue.txt");
-	KnightDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/KnightDialogue.txt");
-	PlayerP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/PlayerP1Dialogue.txt");
-	PlayerP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/PlayerP2Dialogue.txt");
-	PlayerP3Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/PlayerP3Dialogue.txt");
-	WalkerDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/WalkerDialogue.txt");
-	BarDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/HumanDialogue/BarDialogue.txt");
+	ElderP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/ElderP1Dialogue.txt");
+	ElderP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/ElderP2Dialogue.txt");
+	KnightDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/KnightDialogue.txt");
+	PlayerP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/PlayerP1Dialogue.txt");
+	PlayerP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/PlayerP2Dialogue.txt");
+	PlayerP3Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/PlayerP3Dialogue.txt");
+	WalkerDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/WalkerDialogue.txt");
+	BarDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/HumanDialogue/BarDialogue.txt");
 
-	GAIElderP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIElderP1Dialogue.txt");
-	GAIElderP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIElderP2Dialogue.txt");
-	GAIKnightDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIKnightDialogue.txt");
-	GAIPlayerP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIPlayerP1Dialogue.txt");
-	GAIPlayerP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIPlayerP2Dialogue.txt");
-	GAIPlayerP3Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIPlayerP3Dialogue.txt");
-	GAIWalkerDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIWalkerDialogue.txt");
-	GAIBarDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/GAIDialogue/GAIBarDialogue.txt");
+	GAIElderP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/GAIDialogue/GAIElderP1Dialogue.txt");
+	GAIElderP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/GAIDialogue/GAIElderP2Dialogue.txt");
+	GAIKnightDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/ContentGAIDialogue/GAIKnightDialogue.txt");
+	GAIPlayerP1Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/GAIDialogue/GAIPlayerP1Dialogue.txt");
+	GAIPlayerP2Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/GAIDialogue/GAIPlayerP2Dialogue.txt");
+	GAIPlayerP3Dialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/GAIDialogue/GAIPlayerP3Dialogue.txt");
+	GAIWalkerDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/GAIDialogue/GAIWalkerDialogue.txt");
+	GAIBarDialogue = ReadFromFile->ReadFile(FPaths::ProjectDir()+"/Content/GAIDialogue/GAIBarDialogue.txt");
 }
 
 void ATextQuestElder::BeginPlay()
